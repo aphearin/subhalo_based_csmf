18f6dd0173f2b85f2da7c9511da63fe369b769f1
Andrew Hearin <andrew.hearin@yale.edu>
2017-05-17 08:16:24 -0700

    Added module storing Numpy-based kernels of the model
    @andrew-zentner FYI, here's my working prototype of the CLF I told you about at KITP. The method implemented here is to use Arthur Jiang's unevolved subhalo mass function to first MC generate a population of subhalos, including values of their mass "at accretion". Then any stellar-to-halo-mass relation can be used to paint M* values onto both hosts and subs in the normal way. The point is, doing things this way, you can entirely avoid the subhalo incompleteness problems pointed out by @duncandc, but still use a "subhalo-based" model. It's easy to imagine choosing to couple this method with SubhaloPhaseSpace to get "more real" subhalo information into the model, or just using NFWPhaseSpace instead, depending on taste and application.

---
+++ jiang_usmf.py
@@ -0,0 +1,111 @@
+""" Module storing kernel functions for generating Monte Carlo realizations of
+the Jiang & van den Bosch 2014 fitting function for the unevolved subhalo mass function
+"""
+import numpy as np
+from scipy.integrate import quad
+from scipy.stats import poisson
+
+
+__all__ = ('unevolved_subhalo_mass_function', )
+
+
+beta = 5.67
+zeta = 1.19
+gamma1 = 0.13
+alpha1 = -0.83
+gamma2 = 1.33
+alpha2 = -0.02
+
+
+def unevolved_subhalo_mass_function(log10_mu, beta=beta, zeta=zeta,
+        gamma1=gamma1, alpha1=alpha1, gamma2=gamma2, alpha2=alpha2):
+    """ Average number of subhalos of mass mu = Msub/Mhost
+    """
+    mu = 10.**log10_mu
+    prefactor_term1 = gamma1*mu**alpha1
+    prefactor_term2 = gamma2*mu**alpha2
+    prefactor = prefactor_term1 + prefactor_term2
+
+    exparg = beta*mu**zeta
+
+    return prefactor*np.exp(-exparg)
+
+
+def mean_nsub_mu_range(log10_mu_min, log10_mu_max, beta=beta, zeta=zeta,
+        gamma1=gamma1, alpha1=alpha1, gamma2=gamma2, alpha2=alpha2):
+    """
+    """
+    csmf_mu = quad(unevolved_subhalo_mass_function, log10_mu_min, log10_mu_max)[0]
+    return csmf_mu
+
+
+def mean_nsub_vs_mhost(mhost_array, log10_msub_min, log10_mu_max=0., log10_mhost_max=15.5,
+        beta=beta, zeta=zeta, gamma1=gamma1, alpha1=alpha1, gamma2=gamma2, alpha2=alpha2):
+    """
+    """
+    npts_mhost_grid = 100
+    log10_mhost_min = log10_msub_min
+    log10_mhost_grid = np.linspace(log10_mhost_min, log10_mhost_max, npts_mhost_grid)
+    log10_mu_min_grid = log10_msub_min - log10_mhost_grid
+    mean_nsub_grid = list(mean_nsub_mu_range(log10_mu_min, log10_mu_max)
+        for log10_mu_min in log10_mu_min_grid)
+    return np.interp(np.log10(mhost_array), log10_mhost_grid, mean_nsub_grid)
+
+
+def monte_carlo_subhalo_mass(mhost, log10_msub_min, counts):
+    """ For an input system of host halo masses, generate
+    a Monte Carlo realization of the subhalo population whose occupation statistics
+    is determined by Eq. (21) of Jiang & van den Bosch (2014), arXiv:1311.5225.
+
+    Parameters
+    ----------
+    mhost_array : ndarray
+        Numpy array of shape (num_hosts, ) storing the mass in Msun/h
+        of each host halo that will be assigned a distribution of subhalo masses.
+
+    counts_array : ndarray
+        Integer array of shape (num_hosts, ) storing the total number of subhalos
+        more massive than msub_min in each host halo. The sum of the entries
+        in counts_array defines num_total_subs
+
+    Returns
+    --------
+    msub_array : ndarray
+        Numpy array of shape (num_total_subs, ) storing a Monte Carlo realization
+        of the mpeak value of every subhalo.
+
+    """
+    npts_mu_grid = 100
+    log10_mu_min = log10_msub_min - np.log10(mhost)
+    log10_mu_max = 0
+    log10_mu_grid = np.linspace(log10_mu_min, log10_mu_max, npts_mu_grid)[::-1]
+    num_subs_above_mu_min = mean_nsub_mu_range(log10_mu_min, log10_mu_max)
+    num_subs_grid = np.array(list(mean_nsub_mu_range(log10_mu, log10_mu_max)
+        for log10_mu in log10_mu_grid))
+    mu_cdf_grid = num_subs_grid/num_subs_above_mu_min
+
+    uran = np.random.rand(counts)
+    mc_log10_mu = np.interp(uran, mu_cdf_grid, log10_mu_grid)
+    return 10**mc_log10_mu
+
+
+def monte_carlo_subhalo_population(mhost_array, log10_msub_min, log10_mhost_bins):
+    """
+    """
+    mean_nsub = mean_nsub_vs_mhost(mhost_array, log10_msub_min)
+    mc_nsub = poisson.rvs(mean_nsub)
+
+    log10_mhost_array = np.log10(mhost_array)
+    result = np.zeros(np.sum(mc_nsub))
+
+    for i in range(len(log10_mhost_bins)-1):
+        log10_mhost_low, log10_mhost_high = log10_mhost_bins[i], log10_mhost_bins[i+1]
+        mhost_mid = 10**(0.5*(log10_mhost_low + log10_mhost_high))
+        host_halo_mask = (log10_mhost_array >= log10_mhost_low) & (log10_mhost_array < log10_mhost_high)
+        subhalo_masses_ibin = monte_carlo_subhalo_mass(mhost_mid, log10_msub_min,
+            np.sum(mc_nsub[host_halo_mask]))
+        subhalo_mask = np.repeat(host_halo_mask, mc_nsub)
+        result[subhalo_mask] = subhalo_masses_ibin*np.repeat(mhost_array[host_halo_mask],
+            mc_nsub[host_halo_mask])
+
+    return result
