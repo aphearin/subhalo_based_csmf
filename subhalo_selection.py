47550e6e6676b69db7b891e216e5babb9a341929
Andrew Hearin <andrew.hearin@yale.edu>
2017-05-19 09:14:33 -0700

    Added docstring

--- subhalo_selection.py
+++ subhalo_selection.py
@@ -1,11 +1,38 @@
-"""
+""" Moduling storing the Numpy-based kernel function ``select_subhalo_indices``
+that can be used to provide a mapping between satellites onto subhalos
 """
 import numpy as np
+from astropy.utils.misc import NumpyRNGContext
 
 
 def select_subhalo_indices(satellite_host_property,
-        bin_edges, sorted_subhalo_host_property):
-    """
+        bin_edges, sorted_subhalo_host_property, seed=None):
+    """ Calculate an array of integers that can be used as an indexing array
+    into the input subhalos.
+
+    Parameters
+    ----------
+    satellite_host_property : ndarray
+        Numpy array of shape (num_satellites, ) storing, for example,
+        the host halo mass of each model satellite
+
+    bin_edges : ndarray
+        Numpy array of shape (num_bins, ) defining the binning of satellites and subhalos
+
+    sorted_subhalo_host_property : ndarray
+        Monotonically increasing array of shape (num_subhalos, ) storing, for example,
+        the host halo mass of each subhalo.
+        Each subhalo in this array will be treated as a candidate satellite.
+
+    seed : int, optional
+        Random number seed used in the Monte Carlo selection of subhalos.
+        Default is None for stochastic results
+
+    Returns
+    -------
+    subhalo_indices : ndarray
+        Integer array of shape (num_satellites, ) storing the index providing the
+        correspondence between satellites and subhalos
     """
     __ = _check_bins_span_satellite_range(satellite_host_property, bin_edges)
     satellite_bin_indices = np.digitize(satellite_host_property, bin_edges) - 1
@@ -16,7 +43,11 @@ def select_subhalo_indices(satellite_host_property,
 
     high = subhalo_bin_indices[satellite_bin_indices+1]
     low = subhalo_bin_indices[satellite_bin_indices]
-    return np.array(np.floor(np.random.rand(len(low))*(high - low) + low)).astype(int)
+
+    with NumpyRNGContext(seed):
+        uran = np.random.rand(len(low))
+
+    return np.array(np.floor(uran*(high - low) + low)).astype(int)
 
 
 def _check_bins_span_satellite_range(x, bin_edges):
