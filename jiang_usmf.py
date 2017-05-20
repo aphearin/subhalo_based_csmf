34f094852e0a46d68e58692c10b5b2db290dc0b5
Andrew Hearin <andrew.hearin@yale.edu>
2017-05-20 13:51:10 -0700

    Added parameters for optional keyword arguments to facilitate refitting, plus testing notebook

--- jiang_usmf.py
+++ jiang_usmf.py
@@ -9,16 +9,17 @@ from scipy.stats import poisson
 __all__ = ('unevolved_subhalo_mass_function', )
 
 
-beta = 5.67
-zeta = 1.19
-gamma1 = 0.13
-alpha1 = -0.83
-gamma2 = 1.33
-alpha2 = -0.02
+beta_jiang14 = 5.67
+zeta_jiang14 = 1.19
+gamma1_jiang14 = 0.13
+alpha1_jiang14 = -0.83
+gamma2_jiang14 = 1.33
+alpha2_jiang14 = -0.02
 
 
-def unevolved_subhalo_mass_function(log10_mu, beta=beta, zeta=zeta,
-        gamma1=gamma1, alpha1=alpha1, gamma2=gamma2, alpha2=alpha2):
+def unevolved_subhalo_mass_function(log10_mu,
+        beta=beta_jiang14, zeta=zeta_jiang14, gamma1=gamma1_jiang14,
+        alpha1=alpha1_jiang14, gamma2=gamma2_jiang14, alpha2=alpha2_jiang14):
     """ Average number of subhalos of mass mu = Msub/Mhost
     """
     mu = 10.**log10_mu
@@ -31,28 +32,29 @@ def unevolved_subhalo_mass_function(log10_mu, beta=beta, zeta=zeta,
     return prefactor*np.exp(-exparg)
 
 
-def mean_nsub_mu_range(log10_mu_min, log10_mu_max, beta=beta, zeta=zeta,
-        gamma1=gamma1, alpha1=alpha1, gamma2=gamma2, alpha2=alpha2):
+def mean_nsub_mu_range(log10_mu_min, log10_mu_max,
+        beta=beta_jiang14, zeta=zeta_jiang14, gamma1=gamma1_jiang14,
+        alpha1=alpha1_jiang14, gamma2=gamma2_jiang14, alpha2=alpha2_jiang14):
     """
     """
-    csmf_mu = quad(unevolved_subhalo_mass_function, log10_mu_min, log10_mu_max)[0]
-    return csmf_mu
+    params = (beta, zeta, gamma1, alpha1, gamma2, alpha2)
+    return quad(unevolved_subhalo_mass_function, log10_mu_min, log10_mu_max, params)[0]
 
 
-def mean_nsub_vs_mhost(mhost_array, log10_msub_min, log10_mu_max=0., log10_mhost_max=15.5,
-        beta=beta, zeta=zeta, gamma1=gamma1, alpha1=alpha1, gamma2=gamma2, alpha2=alpha2):
+def mean_nsub_vs_mhost(mhost_array, log10_msub_min,
+        log10_mu_max=0., log10_mhost_max=15.5, **kwargs):
     """
     """
     npts_mhost_grid = 100
     log10_mhost_min = log10_msub_min
     log10_mhost_grid = np.linspace(log10_mhost_min, log10_mhost_max, npts_mhost_grid)
     log10_mu_min_grid = log10_msub_min - log10_mhost_grid
-    mean_nsub_grid = list(mean_nsub_mu_range(log10_mu_min, log10_mu_max)
+    mean_nsub_grid = list(mean_nsub_mu_range(log10_mu_min, log10_mu_max, **kwargs)
         for log10_mu_min in log10_mu_min_grid)
     return np.interp(np.log10(mhost_array), log10_mhost_grid, mean_nsub_grid)
 
 
-def monte_carlo_subhalo_mass(mhost, log10_msub_min, counts, randoms=None):
+def monte_carlo_subhalo_mass(mhost, log10_msub_min, counts, randoms=None, **kwargs):
     """ For an input host halo mass, generate a Monte Carlo realization
     of the Mpeak values of a subhalo population according to the unevolved mass function
     given by Eq. (21) of Jiang & van den Bosch (2014), arXiv:1311.5225.
@@ -85,8 +87,8 @@ def monte_carlo_subhalo_mass(mhost, log10_msub_min, counts, randoms=None):
     log10_mu_min = log10_msub_min - np.log10(mhost)
     log10_mu_max = 0
     log10_mu_grid = np.linspace(log10_mu_min, log10_mu_max, npts_mu_grid)[::-1]
-    num_subs_above_mu_min = mean_nsub_mu_range(log10_mu_min, log10_mu_max)
-    num_subs_grid = np.array(list(mean_nsub_mu_range(log10_mu, log10_mu_max)
+    num_subs_above_mu_min = mean_nsub_mu_range(log10_mu_min, log10_mu_max, **kwargs)
+    num_subs_grid = np.array(list(mean_nsub_mu_range(log10_mu, log10_mu_max, **kwargs)
         for log10_mu in log10_mu_grid))
     mu_cdf_grid = num_subs_grid/num_subs_above_mu_min
 
@@ -99,23 +101,24 @@ def monte_carlo_subhalo_mass(mhost, log10_msub_min, counts, randoms=None):
     return 10**mc_log10_mu
 
 
-def monte_carlo_subhalo_population(mhost_array, log10_msub_min, log10_mhost_bins):
+def monte_carlo_subhalo_population(mhost_array, log10_msub_min, log10_mhost_bins,
+        **kwargs):
     """
     """
-    mean_nsub = mean_nsub_vs_mhost(mhost_array, log10_msub_min)
+    mean_nsub = mean_nsub_vs_mhost(mhost_array, log10_msub_min, **kwargs)
     mc_nsub = poisson.rvs(mean_nsub)
 
     log10_mhost_array = np.log10(mhost_array)
-    result = np.zeros(np.sum(mc_nsub))
+    mc_subhalo_mpeak = np.zeros(np.sum(mc_nsub))
 
     for i in range(len(log10_mhost_bins)-1):
         log10_mhost_low, log10_mhost_high = log10_mhost_bins[i], log10_mhost_bins[i+1]
         mhost_mid = 10**(0.5*(log10_mhost_low + log10_mhost_high))
         host_halo_mask = (log10_mhost_array >= log10_mhost_low) & (log10_mhost_array < log10_mhost_high)
         subhalo_masses_ibin = monte_carlo_subhalo_mass(mhost_mid, log10_msub_min,
-            np.sum(mc_nsub[host_halo_mask]))
+            np.sum(mc_nsub[host_halo_mask]), **kwargs)
         subhalo_mask = np.repeat(host_halo_mask, mc_nsub)
-        result[subhalo_mask] = subhalo_masses_ibin*np.repeat(mhost_array[host_halo_mask],
-            mc_nsub[host_halo_mask])
+        mc_subhalo_mpeak[subhalo_mask] = subhalo_masses_ibin*np.repeat(
+            mhost_array[host_halo_mask], mc_nsub[host_halo_mask])
 
-    return result
+    return mc_nsub, mc_subhalo_mpeak
